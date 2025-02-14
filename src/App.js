f35aae71a8e137502703ec66182eb79fe1d93da9
Catherine <cmccorkindale911@gmail.com>
2025-02-14 09:11:53 +0000

    Foundations for Background Pink/Purp Blurs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// Background Component
+const Background = () => {
+  return (
+    <div className="gradient-bg">
+      <div className="gradients-container">
+        <div className="g1"></div>
+        <div className="g2"></div>
+        <div className="g3"></div>
+        <div className="g4"></div>
+        <div className="g5"></div>
+        <div className="interactive"></div>
+      </div>
+    </div>
+  );
+};
+
+// Main App Component
 function App() {
   return (
     <div className="App">
+      <Background />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Content goes here */}
       </header>
     </div>
   );
