99b72f7969260b7548c5374d8685331a50c401a6
Catherine <cmccorkindale911@gmail.com>
2025-02-15 21:41:38 +0000

    added functionality to emails in footer

--- src/HomePage/App.js
+++ src/HomePage/App.js
@@ -145,7 +145,7 @@ const Foot = () => {
   return (
     <div className='footer'>
       <p>Created by Women@CS - Empowering Women in Tech</p>
-      <p><a href="#privacy-policy">cm2165@hw.ac.uk</a> | <a href="#terms">po2010@hw.ac.uk</a></p>
+      <p><a href="mailto:cm2165@hw.ac.uk?cc=po2010@hw.ac.uk">cm2165@hw.ac.uk</a> | <a href="mailto:po2010@hw.ac.uk?cc=cm2165@hw.ac.uk">po2010@hw.ac.uk</a></p>
     </div>
   );
 };
