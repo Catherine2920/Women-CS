03af94d1016aa3cf64fecf217fbebeb775d7d420
Catherine <cmccorkindale911@gmail.com>
2025-02-15 19:37:30 +0000

    about us added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import logo from './Logo.png';
 import logoName from './logoName.png';
+import fakeQRCode from './FakeQRCode.png';
 
 const Background = () => {
   const [animationStarted, setAnimationStarted] = useState(false);
@@ -109,6 +110,30 @@ return(
     <h1>Tell us how to improve</h1>
     <button onClick={handleClick}>Help Us Learn</button>
   </div>
+
+  {/*JOIN OUR GROUP*/}
+  <div className="JoinGroupSqaure">
+      <h1>
+        Join Our <span className="GroupChat">Group Chat</span>!
+      </h1>
+      <img src={fakeQRCode} alt="Profile" className="fakeQRcode" />
+    </div>
+
+    {/*ABOUT US*/}
+
+    <div className="AboutUs">
+  <p>
+    We are a <span className="highlight">student-led group</span> dedicated to supporting and 
+    <span className="highlight"> empowering women.</span> 
+    Our mission is to foster a strong, inclusive community where members can build confidence 
+    in their technical skills, connect with like-minded peers, and 
+    <span className="highlight"> gain industry insights.</span> 
+    We achieve this through a range of initiatives, including mentorship programs, technical
+    workshops, networking events, and hackathons.
+  </p>
+</div>
+
+
 
 
   </div>
